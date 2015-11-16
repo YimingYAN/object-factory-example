fe8ee8af1005eac7ae1152cc694bdb25ea3b503c
Yiming Yan <yiming.yan@sheffield.ac.uk>
2015-11-16 10:14:00 +0000

    clean up

--- iconcretefactory.h
+++ iconcretefactory.h
@@ -6,9 +6,6 @@ class IConcreteFactory
 public:
     IConcreteFactory();
     virtual Base* create(Base* parent)  { Q_UNUSED(parent); return 0; }
-    virtual Base* create(QString objName, Base* parent) { Q_UNUSED(objName);
-                                                          Q_UNUSED(parent);
-                                                          return 0; }
 };
 
 #endif // ICONCRETEFACTORY_H
